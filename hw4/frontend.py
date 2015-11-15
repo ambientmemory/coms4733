8ce7bdf289b2493e19a6760c10228a12466ca37f
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2015-11-15 14:29:33 -0500

    got a polygon to appear

--- hw4/frontend.py
+++ hw4/frontend.py
@@ -1,5 +1,35 @@
 __author__ = 'Piyali'
 
 import tkinter
-top = tkinter.Tk()
-top.mainloop()
+from tkinter import *
+
+class Visual(Frame):
+
+    def __init__(self,parent):
+        #initialize frame
+        Frame.__init__(self,parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Map")
+        #setting frame params
+        self.pack(fill=BOTH, expand=1)
+
+        #start rendering here
+        canvas = Canvas(self)
+
+        #initialize coordinates for polygon vertices
+        points = [150,100, 200, 120, 240, 180, 210, 200, 150, 150, 100, 200 ]
+        canvas.create_polygon(points, outline='black', fill='gray', width=2)
+
+        canvas.pack(fill=BOTH, expand=1)
+
+def main():
+    gui_tk = Tk()
+    visualizer = Visual(gui_tk)
+    gui_tk.geometry("500x500+300+300")
+    gui_tk.mainloop()
+
+if __name__ == '__main__':
+    main()
