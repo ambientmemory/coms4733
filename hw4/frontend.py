faed876b70358e6cfff2cf792c0946c681ed74eb
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2015-11-17 22:24:04 -0500

    waiting for the Jettness

--- hw4/frontend.py
+++ hw4/frontend.py
@@ -30,22 +30,36 @@ class Visual(Frame):
 
     def drawer(self,canvas):
         obstacle_filename = open('hw4_world_and_obstacles_convex.txt')
-        list_of_objects = []        #This will be a list of lists, each element represents an object, each object in turn is a list of points
+        list_of_objects = []
+        #This will be a list of lists, each element represents an object, each object in turn is a list of points
+
         # We first read the count of objects
         count_of_objects = int(obstacle_filename.readline().strip()) # read first line
+
         for i in range(count_of_objects):
-            count_of_edges = int(obstacle_filename.readline().strip()) # read first line of the object description
-            list_of_points = []                                                        # The parameters to create_polygon are x0,y0,x1,y1...
+            count_of_edges = int(obstacle_filename.readline().strip())
+            # read first line of the object description
+
+            list_of_points = []
+            # The parameters to create_polygon are x0,y0,x1,y1...
+
             for j in range(count_of_edges):
                 line = obstacle_filename.readline()
                 point_data = line.split()
                 list_of_points.append(scale_x*(tr_x - float(point_data[1])))
                 list_of_points.append(scale_y*(tr_y - float(point_data[0])))
-            list_of_objects.append(list_of_points)              # This will append the list of points of the current object to the list of objects
+            #endfor
 
+            list_of_objects.append(list_of_points)
+            # This will append the list of points of the current object to the list of objects
+
+        # renders outside map
         canvas.create_polygon(list_of_objects[0], outline='black', fill='white', width=3.0)
+
+        #renders inner objects
         for i in range(1,count_of_objects):
             canvas.create_polygon(list_of_objects[i], outline='blue', fill='white', width=0.5)
+
         canvas.pack(fill=BOTH, expand=1)
 
 
