4138d1c6842632f9caa9f5944eec33bb11170b9d
Tia Zhao <tiazhao112@gmail.com>
2015-11-18 00:07:05 -0500

    tested dijkstra - works now!

--- hw4/dijkstra.py
+++ hw4/dijkstra.py
@@ -1,16 +1,36 @@
 from visibility import *
 
 ''' 
-sample graph
-graph = {
-    "a" : [("b", 1), ("c", 2), ("d", 3)],
-    "b" : [("a", 1), ("c", 1), ("e", 2)],
-    "c" : [("a", 2), ("b", 1), ("d", 1), ("e", 3), ("f", 2)],
-    "d" : [("a", 3), ("c", 1), ("f", 2)],
-    "e" : [("b", 2), ("c", 3), ("f", 1), ("t", 2)],
-    "f" : [("c", 3), ("d", 2), ("e", 1), ("t", 2)],
-    "t" : [("e", 2), ("f", 2)]
-    }
+Test on sample graph
+Path should be 0 -> 1 -> 2 -> 3
+
+graph = []
+points = []
+points.append(Point(0,0))
+points.append(Point(1,0))
+points.append(Point(3,0))
+points.append(Point(5,0))
+points.append(Point(0,3))
+points.append(Point(1,1))
+points.append(Point(2,2))
+
+for i in range(len(points)):
+    graph.append(Vertex(points[i],i,-1))
+
+graph[0].addAdjacent(graph[1])
+graph[0].addAdjacent(graph[4])
+graph[0].addAdjacent(graph[5])
+graph[1].addAdjacent(graph[5])
+graph[1].addAdjacent(graph[2])
+graph[2].addAdjacent(graph[3])
+graph[2].addAdjacent(graph[6])
+graph[3].addAdjacent(graph[6])
+graph[4].addAdjacent(graph[5])
+graph[4].addAdjacent(graph[6])
+graph[5].addAdjacent(graph[6])
+
+dijkstra(graph, graph[0], graph[3])
+
 '''
 
 def dijkstra(vertices, start, target):
@@ -21,13 +41,13 @@ def dijkstra(vertices, start, target):
         graph[v.index] = []
         
         for i in v.adjacent:
-            dist = ((v.p.x - i.p.x)**2 + (v.p.y - i.p.y)**2)**.5  # distance
+            dist = ((v.p.x - i.p.x)**2 + (v.p.y - i.p.y)**2)**.5
             graph[v.index].append((i.index, dist))
     
     
     visited = {start.index: 0}
     unvisited = {}
-    path = {}
+    prev = {}
     
     for i in graph:
         if(i != start.index):
@@ -40,24 +60,19 @@ def dijkstra(vertices, start, target):
         for x in graph[n]:
             if(x[0] in unvisited and (unvisited[x[0]] > x[1] + visited[n] or unvisited[i] < 0)):
                 unvisited[x[0]] = x[1] + visited[n]
-                path[x[0]] = n
+                prev[x[0]] = n
                 
         n =  min(unvisited)
         visited[n] = unvisited[n]
         unvisited.pop(n)
     
-    print(visited) # delete later
-    print(path)
     
-    p = [target]
-    y = target
-    while y!= start:
-        p.append(path[y])
-        y = path[y]
+    path = [target.index]
+    y = target.index
+    while y!= start.index:
+        path.append(prev[y])
+        y = prev[y]
         
-    p.reverse()
-    print(p)
-    return p
-
-
-#dijkstra(graph, "a", "t")
+    path.reverse()
+#    print(path)
+    return path
